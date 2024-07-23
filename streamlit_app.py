a4a241a3dbd582c1c3e20e9a393a5eff104dc95c
lk <108403993+lkNGAT@users.noreply.github.com>
2024-07-23 13:59:31 +0200

    Update streamlit_app.py
    Change pos

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data =fruityvice_response.json(), use_container_width=True )
+
 
 # Write directly to the app
 st.title("Customize your smoothie :cup_with_straw:")
@@ -21,6 +19,9 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data =fruityvice_response.json(), use_container_width=True )
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 if ingredients_list:
