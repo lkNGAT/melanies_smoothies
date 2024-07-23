596e570e24a575799c49437ec85d3d9cea4d9523
lk <108403993+lkNGAT@users.noreply.github.com>
2024-07-23 14:01:30 +0200

    Update streamlit_app.py
    Load api data for each ingredient

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,15 +18,15 @@ st.write('The name on you smoothie will be: ',name_on_order)
 cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data =fruityvice_response.json(), use_container_width=True )
+
+
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 if ingredients_list:
     ingredients_string = ''
     for x in ingredients_list:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data =fruityvice_response.json(), use_container_width=True )
         ingredients_string += x + ' '
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
