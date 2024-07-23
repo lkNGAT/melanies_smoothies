80f0c2e9e93cadb24f9880e0bb56f32369bab904
lk <108403993+lkNGAT@users.noreply.github.com>
2024-07-23 13:58:52 +0200

    Update streamlit_app.py
    Add response df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
 import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data =fruityvice_response.json(), use_container_width=True )
 
 # Write directly to the app
 st.title("Customize your smoothie :cup_with_straw:")
